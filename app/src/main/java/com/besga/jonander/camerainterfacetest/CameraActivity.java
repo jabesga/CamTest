dc5c31c272f67dcdbd5e2a59236bf0e1fc7a09db
Jon Ander <jonan.bsg@gmail.com>
2016-02-04 21:31:20 +0100

    fix camera orientation left

--- app/src/main/java/com/besga/jonander/camerainterfacetest/CameraActivity.java
+++ app/src/main/java/com/besga/jonander/camerainterfacetest/CameraActivity.java
@@ -1,18 +1,27 @@
 package com.besga.jonander.camerainterfacetest;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
 import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -36,10 +45,26 @@ public class CameraActivity extends Activity {
     private Camera mCamera;
     private CameraPreview mPreview;
 
+    // This snippet hides the system bars.
+    private void hideSystemUI() {
+        // Set the IMMERSIVE flag.
+        // Set the content to appear under the system bars so that the content
+        // doesn't resize when the system bars hide and show.
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hideSystemUI();
         setContentView(R.layout.activity_camera);
+
 
         if(!checkCameraHardware(this)){
             return;
@@ -56,25 +81,20 @@ public class CameraActivity extends Activity {
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(mPreview);
 
-        /** PRUEBA DE OVERLAY **/
+        /** PRUEBA DE OVERLAY **//*
         LayoutInflater controlInflater = LayoutInflater.from(getBaseContext());
         View viewControl = controlInflater.inflate(R.layout.overlay_camera, null);
         ViewGroup.LayoutParams layoutParamsControl = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT);
-        this.addContentView(viewControl, layoutParamsControl);
+        this.addContentView(viewControl, layoutParamsControl);*/
         /** PRUEBA DE OVERLAY **/
 
-
-        // Add a listener to the Capture button
-        Button captureButton = (Button) findViewById(R.id.button_capture);
-        captureButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // get an image from the camera
-                        mCamera.takePicture(shutterCallback, null, mPicture);
-                    }
-                }
-        );
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            mCamera.takePicture(shutterCallback, null, mPicture);
+        }
+        return super.onTouchEvent(event);
     }
 
     /** Check if this device has a camera */
@@ -115,6 +135,21 @@ public class CameraActivity extends Activity {
                 FileOutputStream fos = new FileOutputStream(pictureFile);
                 fos.write(data);
                 fos.close();
+
+                /** TURN IMAGE AFTER SAVE **/
+                Bitmap bm = BitmapFactory.decodeFile(pictureFile.getAbsolutePath());
+
+                Matrix matrix = new Matrix();
+                matrix.postRotate(90);
+                Bitmap rotatedBitmap = Bitmap.createBitmap(bm, 0, 0, bm.getWidth(), bm.getHeight(), matrix, true);
+
+                fos = new FileOutputStream(pictureFile);
+                rotatedBitmap.compress(Bitmap.CompressFormat.JPEG, 90, fos);
+                fos.close();
+
+                mPreview.restartCamera();
+                /** TURN IMAGE AFTER SAVE **/
+
                 Log.d(TAG, "Picture saved in: " + Uri.fromFile(pictureFile).toString());
 
             } catch (FileNotFoundException e) {
